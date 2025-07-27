59e26b5abd7a9839befe34dafd455d908ca9f120
German Lungo <g.lungo.m@gmail.com>
2025-07-27 00:08:51 -0300

    Delete build directory

--- build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/German Lungo/source/repos/glungo/cpp-ecs-demo
-# Build directory: C:/Users/German Lungo/source/repos/glungo/cpp-ecs-demo/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("engine")
-subdirs("test_app")
-subdirs("engine-tests")
